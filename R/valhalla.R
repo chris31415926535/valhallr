6e4c39c21e2478898aa988ad41ca92fb4024a0f9
chris b <christopher.a.belanger@gmail.com>
2021-05-07 08:44:03 -0400

    Updates and bugfixes for valhallr::od_table()
    * Fixed bug where batching would do one too many iterations in some cases.
    * Removed a major bottleneck in the final step that puts batch results back into one tibble. Now uses a for-loop and modify-in-place instead of copy-on-modify, for >200x speed gain in testing.
    * Added basic input validation (check for `lat` and `lon` columns).
    * Rewrote to use less metaprogramming stuff like {{}} := . Now renames user-supplied columns once at the beginning, works with standard names, then sets them back at the end.
    * Added some more comments.

--- R/valhalla.R
+++ R/valhalla.R
@@ -360,30 +360,37 @@ sources_to_targets <- function(froms, tos, costing = "auto",from_search_filter =
 od_table <- function(froms, from_id_col, tos, to_id_col, costing = "auto", batch_size = 100, minimum_reachability = 500, verbose = FALSE, hostname = "localhost", port = 8002){
   # note: got importFrom rlang trick here: https://stackoverflow.com/questions/58026637/no-visible-global-function-definition-for
   from_index <- to_index <- NULL
-  # FIXME TODO: do input validation!!
+
+  # Basic input validation
+  if ((!"lat" %in% colnames(froms)) | (!"lon" %in% colnames(tos))) stop("Input data `froms` and `tos` must both have columns named `lat` and `lon`. ")
+
+  # temporarily rename the data columns so they're easier to work with
+  # we'll put the names back right at the end
+  froms <- rename(froms, .from_id_col = {{from_id_col}})
+  tos   <- rename(tos, .to_id_col = {{to_id_col}})
 
   # get the human-readable names of the from- and to-data
   from_names <- froms %>%
-    dplyr::select(from_id_col) %>%
+    dplyr::select(.from_id_col) %>%
     tibble::rowid_to_column(var = "from_index")
 
   to_names <- tos %>%
-    dplyr::select(to_id_col) %>%
+    dplyr::select(.to_id_col) %>%
     tibble::rowid_to_column(var = "to_index")
 
   # if human-readable column names are identical, append "_from" and "_to" so they differ
   if (from_id_col == to_id_col) {
-    new_from <- paste0(from_id_col,"_from")
-    from_names <- dplyr::rename(from_names, !!(new_from) := from_id_col)
-    from_id_col <- new_from
+    #new_from <- paste0(from_id_col,"_from")
+    #from_names <- dplyr::rename(from_names, !!(new_from) := from_id_col)
+    from_id_col <- paste0(from_id_col,"_from")
 
-    new_to <- paste0(to_id_col, "_to")
-    to_names <- dplyr::rename(to_names, !!(new_to) := to_id_col)
-    to_id_col <- new_to
+    #new_to <- paste0(to_id_col, "_to")
+    #to_names <- dplyr::rename(to_names, !!(new_to) := to_id_col)
+    to_id_col <- paste0(to_id_col, "_to")
   }
 
   # set up our batching
-  n_iters <- nrow(froms) %/% batch_size + 1
+  n_iters <- ceiling(nrow(froms) / batch_size)
   results <- list(rep(NA, n_iters))
 
   # do each batch
@@ -394,6 +401,7 @@ od_table <- function(froms, from_id_col, tos, to_id_col, costing = "auto", batch
 
     froms_iter = froms[start_index:end_index, ] %>%
       tidyr::drop_na()
+
     od <- valhallr::sources_to_targets(froms = froms_iter,
                                        tos = tos,
                                        costing = costing,
@@ -401,29 +409,63 @@ od_table <- function(froms, from_id_col, tos, to_id_col, costing = "auto", batch
                                        hostname = hostname,
                                        port = port)
 
-    # FIXME TODO: confirm that sources_to_targets gave us meaningful data!
+    # POTENTIAL TODO: validate the API response from sources_to_targets()
 
     # make start_index match the original DB row number and doc row number
     od <- od %>%
       dplyr::mutate(from_index = from_index + start_index,
-             to_index = to_index + 1) %>%
+                    to_index = to_index + 1) %>%
       dplyr::left_join(from_names, by = "from_index") %>%
       dplyr::left_join(to_names, by = "to_index") %>%
       dplyr::select(-to_index, -from_index)
 
     # add results to our pre-built list
     results[[i]] <- od
-
   }
 
-  # get results back into a tibble
-  output <- results %>%
-    tibble::enframe() %>%
-    tidyr::unnest("value") %>%
-    dplyr::select(from_id_col, to_id_col, "distance", "time")
+  # get results from a list of tibbles back into a single tibble
+  # this has been rewritten speed and memory optimization
+  # originally it was much shorter and used tidyr::unnest() but it was a major
+  # bottleneck for large datasets (and would even fail)
 
+  # get info about our results
+  n_results <- length(results)
+  n_rows <- purrr::map_dbl(results, nrow)
+  total_rows <- sum(n_rows)
+
+  # set up our final output tibble
+  output <- tibble::tibble(.from_id_col = rep(NA, total_rows),
+                           .to_id_col = rep(NA, total_rows),
+                           distance = rep(NA_real_, total_rows),
+                           time = rep(NA_real_, total_rows))
+
+  # loop through each batch of results, move the results from the batch to the
+  # final output tibble. a loop is much faster than using tidyr::unnest() here
+  # because it modifies in place and isn't binding rows.
+  for (i in 1:n_results) {
+
+    # set our start and end indices
+    index_start <- 1
+    index_end <- n_rows[[1]]
+    if (i != 1){
+      index_start <- sum(n_rows[1:(i-1)]) + 1
+      index_end <- sum(n_rows[1:i])
+    }
+
+    # overwrite the relevant bits of the vectors
+    output$distance[index_start:index_end] <- results[[i]]$distance
+    output$time[index_start:index_end] <- results[[i]]$time
+    output$.from_id_col[index_start:index_end] <- results[[i]]$.from_id_col
+    output$.to_id_col[index_start:index_end] <- results[[i]]$.to_id_col
+  }
+
+  # set the names back
+  output <- dplyr::rename(output,
+                          {{from_id_col}} := .from_id_col,
+                          {{to_id_col}} := .to_id_col)
+
+  # voila
   return(output)
-
 }
 
 
